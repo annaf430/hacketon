fada476ae937dd43f4046de97d84b4aa12e8f976
unknown <u9251391@bsmch.net>
2023-12-17 15:57:18 +0200

    upload files

---
+++ Pages/JustInCase/Privacy.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Hackaton8Website.Pages.JustInCase
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
